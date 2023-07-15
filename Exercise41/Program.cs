9ac6804c7a5c7656dcf9125f641a7af2a7d86b6d
LyubovS <l.sotskova@gmail.com>
2023-07-15 18:38:13 +0300

    Add Exercise 41

---
+++ Exercise41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите размер массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[" + "\t");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.Write("]");
+}
+
+int PositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] result = GetArray(M);
+PrintArray(result);
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел в массиве --> {PositiveNumbers(result)}");
